496a41e071910bba8946bde173ac06b31640fe3c
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-23 11:28:11 -0500

    finshes structing models for both post and user created userData seed file ;

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,14 @@
 // Seeds requires
 const sequelize = require('../config/connection');
 const seedPost = require('./postData');
+const seedUser = require('./userData');
 
 const seedALL = async () => {
     await sequelize.sync({ focre: true});
 
     await seedPost();
+
+    await seedUser(); 
 };
 
 seedALL();
