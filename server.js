c232fda5fba7e456d9a1d62477d47f8241826eff
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-22 21:48:49 -0500

    installed nodemon and updated package.json

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+// Requires
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./helpers');
+// const helpers = require('./helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
