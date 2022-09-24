25c10b1ce6c3e5f89a964f84697c7f75e7a8ec46
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-24 10:56:41 -0500

    link controllers folder to server.js add new file in controller (home-routes.js) add comments to server.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 // Requires
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
 
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // const helpers = require('./helpers');
 
+// Calling express and establishing a port
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Links to the controllers
+app.use(routes);
 
 sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
