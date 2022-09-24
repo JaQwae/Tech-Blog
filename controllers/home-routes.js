93cca242d87cf1dcf9e64c6b7bf0574c2893a3e7
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-24 12:40:02 -0500

    creation of api/index and user-routes.js add login get route to home routes added api routes to index.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,17 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.send ('Homepage route established')
+        res.send ('Homepage routes established')
 
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', async (req, res) => {
+    try {
+        res.send('Login route established')
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api/user-routes');
 
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
