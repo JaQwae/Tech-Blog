72a71db7a70d3e692553a9bb9aa0df8f24d03348
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-24 13:22:32 -0500

    added requires and middleware for handlebars in server.js create utils folder and added helpers.js in there

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-// Requires
+// npm requires
 const path = require('path');
 const express = require('express');
 // const session = require('express-session');
-// const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
+// file/folder requires
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const helpers = require('./helpers');
+const helpers = require('./utils/helpers');
 
 // Calling express and establishing a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Handlebars middleware
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Links to the controllers
 app.use(routes);
