60c98b484e6c401a88091d26b23f68c6f7abbfc0
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-25 20:55:16 -0500

    console log on respone in login.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,22 +4,21 @@ const { User } = require('../../models');
 // CREATE new user
 router.post('/', async (req, res) => {
     try {
-        res.send('create route')
-        // const dbUserData = await User.create({
-        //     username: req.body.username,
-        //     email: req.body.email,
-        //     password: req.body.password,
-        // });
+        const dbUserData = await User.create({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
-        // // Set up sessions with a 'loggedIn' variable set to `true`
-        // req.session.save(() => {
-        //     req.session.loggedIn = true;
+        // Set up sessions with a 'loggedIn' variable set to `true`
+        req.session.save(() => {
+            req.session.loggedIn = true;
 
-        //     res.status(200).json(dbUserData);
-        // });
+            res.status(200).json(dbUserData);
+        });
     } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        console.log("post err = " + err);
+        res.status(400).json(err);
     }
 });
 
@@ -27,7 +26,7 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         // find user who matched the search
-        const userData = await User.findOne({ where: { username: req.body.username } });
+        const userData = await User.findOne({ where: { name: req.body.username } });
         
         if (!userData) {
             res
--- public/js/login.js
+++ public/js/login.js
@@ -14,13 +14,39 @@ const loginFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('http://localhost:3001/');
         } else {
             alert('Login failed');
         }
     }
 }
 
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#sign-up-username').value.trim();
+    const email = document.querySelector('#sign-up-email').value.trim();
+    const password = document.querySelector('#sign-up-password').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('http://localhost:3001/api/user/', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+console.log (response)
+        if (response.ok) {
+            document.location.replace('http://localhost:3001/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
 document
     .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.sign-up-form')
     .addEventListener('submit', loginFormHandler);
