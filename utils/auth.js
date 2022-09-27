e049595d9cd66547d52da3a8e549189fee174514
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-26 22:24:09 -0500

    add auth.js in the utils folder and created withAuth

---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const withAuth = (req, res, next) => {
+    // If  user not logged in, redirect to the login page
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        // if logged in, execute the route
+        next();
+    }
+}
