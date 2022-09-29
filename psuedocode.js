bd95bceec8f4fc99221be4e8431ce1aaa48615bf
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-28 19:14:17 -0500

    created dashboard and single post handlebar file

--- psuedocode.js
+++ psuedocode.js
@@ -17,7 +17,7 @@
 // THEN I am prompted to create a username and password
 
 
-// WHEN I click on the sign-up button
+// *WHEN I click on the sign-up button
 // THEN my user credentials are saved and I am logged into the site
 
 
@@ -25,16 +25,17 @@
 // THEN I am prompted to enter my username and password
 
 
-// WHEN I am signed in to the site
+// *WHEN I am signed in to the site
 // THEN I see navigation links for the homepage, the dashboard, and the option to log out
 
 
-// WHEN I click on the homepage option in the navigation
+// *WHEN I click on the homepage option in the navigation
 // THEN I am taken to the homepage and presented with existing blog posts that include the post title and the date created
 
 
 // WHEN I click on an existing blog post
 // THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+
 
 
 // WHEN I enter a comment and click on the submit button while signed in
