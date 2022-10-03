d4852ae2de47178236584c022631a21e8a6d6779
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-02 20:53:37 -0500

    debgging delete functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -30,7 +30,7 @@ router.put('/:id', withAuth, async (req, res) => {
         })
 
         if (!postData) {
-            res.status(404).json({ message: 'No project found with this id!' });
+            res.status(404).json({ message: 'No post found with this id!' });
             return;
         }
         res.status(200).json(postData);
@@ -41,19 +41,18 @@ router.put('/:id', withAuth, async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const postData = await Post.destroy({
+        const postData = await Post.destroy(req.body,{
             where: {
                 id: req.params.id,
-                user_id: req.session.user_id,
             },
         });
 
         if (!postData) {
-            res.status(404).json({ message: 'No project found with this id!' });
+            res.status(404).json({ message: 'No post found with this id!' });
             return;
         }
 
-        res.status(200).json(projectData);
+        res.status(200).json(postData);
     } catch (err) {
         res.status(500).json(err);
     }
