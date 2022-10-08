e82ddd5adad677ebf9b7df806c3243c8e5b1c9ff
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-08 13:47:24 -0500

    changes to server

--- server.js
+++ server.js
@@ -53,10 +53,7 @@ app.use(express.urlencoded({ extended: true }));
 // Links to the controllers
 app.use(routes);
 
-sequelize.sync({ force: false}).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
 });
