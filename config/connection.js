e70dc32ad933d0e4281e5ce2158c552341b1a24e
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-08 12:50:01 -0500

    server chage

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,45 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-    }
-);
+let sequelize;
 
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+        host:'au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        }
+    })
+}
+else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
 module.exports = sequelize;
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306,
+//     }
+// );
+
+// module.exports = sequelize;
+
