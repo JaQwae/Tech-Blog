e6e3e3b225042a8d164ce10e7dcee6ed4751fc2e
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-08 13:16:43 -0500

    commit message

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,46 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
-
-if(process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
-        host:'au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
         dialect: 'mysql',
-        dialectOptions: {
-            decimalNumbers: true,
-        }
-    })
-}
-else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306,
-        }
-    );
-}
+        port: 3306
+    });
+
 module.exports = sequelize;
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if(process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL, {
+//         host:'au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//         dialect: 'mysql',
+//         dialectOptions: {
+//             decimalNumbers: true,
+//         }
+//     })
+// }
+// else {
+//     sequelize = new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASSWORD,
+//         {
+//             host: 'localhost',
+//             dialect: 'mysql',
+//             port: 3306,
+//         }
+//     );
+// }
+// module.exports = sequelize;
 
 
 // const Sequelize = require('sequelize');
